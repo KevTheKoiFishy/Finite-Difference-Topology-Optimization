287073035a48d60b7774de694226fc2462b55846
Kevi <KevTheKoiFishy@Starship>
2025-10-08 16:12:37 -0400

    🤯 Crazy big sim

--- animation_res.py
+++ animation_res.py
@@ -1,11 +1,14 @@
 import json
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from   matplotlib.animation import FuncAnimation
 
 # Create initial data
-with open("sim_results.json", "r") as res:
-    data = np.array(json.loads(res.read()))
+data = pd.read_feather("sim_results.feather").values
+with open("sim_settings.json", "r") as set_json:
+    settings = json.loads(set_json.read())
+sim_dims = settings["dim"]
 
 lower = np.min(data)
 upper = np.max(data)
@@ -15,20 +18,20 @@ print(f"Data Bounds: [{lower}, {upper}]")
 
 # Set up the figure and initial imshow plot
 fig, ax = plt.subplots()
-im = ax.imshow(data[0], vmin = lower, vmax = upper, cmap='RdPu', animated=True)
+im = ax.imshow(data[0].reshape(sim_dims), vmin = lower, vmax = upper, cmap='RdPu', animated=True)
 fig.colorbar(im)
 frame_text = ax.text(0, 1.05, '', transform=ax.transAxes, ha='left', va='top') # Text object for frame number
 
 # Define the update function for each frame
 def update(frame):
-    new_data = data[frame]
+    new_data = data[frame].reshape(sim_dims)
     im.set_array(new_data)
     frame_text.set_text(f'Frame: {frame}')
 
     return [im]
 
 # Create the animation
-ani = FuncAnimation(fig, update, frames=range(0, len(data), 10), interval=10, blit=False)
+ani = FuncAnimation(fig, update, frames=np.unique(np.uint32(np.logspace(0, np.log10(len(data)), 200)))[:-1], interval=10, blit=False)
 
 # Show the animation
 plt.show()
